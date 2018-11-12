d3360e64c532e15311be8145b0975c3b4b93d90a
nikolkats <nikolettakats@gmail.com>
2018-11-12 12:34:51 +0100

    register page

--- frontend/src/components/views/register.js
+++ frontend/src/components/views/register.js
@@ -8,9 +8,61 @@ import ReactDOM from 'react-dom'
 
 
 class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {first_name: '', last_name: '', username: '', password: '', email: '', phone: 0, address: '', number: 0, post_code: 0, country: '', birth_date: ''};
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleSubmit () {
+       const us = document.getElementById('username').value;
+       const pass = document.getElementById('pwd').value;
+       
+       this.setState(() => ({ username: us, password: pass}));
+        
+    }
     render() {
         return (
-                <h1> Register Page </h1>
+            <form onSubmit={this.handleSubmit}>
+            <div>
+                </div>
+                <div class="form-group">
+                    <label for="first_name">First Name:</label>
+                    <input id="first_name" name="first_name" class="form-control" type="text" required/>
+                </div>
+                
+                <div class="form-group">
+                    <label for="last_name">Last Name:</label>
+                    <input id="last_name" name="last_name" class="form-control" type="text" required/>
+                </div>
+                
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" size="30" required/>
+                </div>
+                
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input id="username" name="username" class="form-control" type="text" required/>
+                </div>
+
+                <div class="form-group">
+                    <label for="pwd">Password:</label>
+                    <input type="password" name="password" class="form-control" id="pwd" required></input>
+                </div>
+                
+                <div class="form-group">
+                    <label for="pwd_repeat">Repeat Password:</label>
+                    <input id="re_pass" name="re_pass" class="form-control" type="password" required/>
+                </div>
+                
+                <div class="form-group">
+                    <label for="birth_date">Date of Birth:</label>
+                    <input type="date" id="birth_date" name="birth_date" class="form-control"  required/>
+                </div>
+                
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
         );
   }
 }
