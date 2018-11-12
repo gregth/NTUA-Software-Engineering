f0772c4fc7ab5b9025406d300750b942016965d3
nikolkats <nikolettakats@gmail.com>
2018-11-12 19:48:25 +0100

    register fotm

--- frontend/src/components/views/register.js
+++ frontend/src/components/views/register.js
@@ -5,63 +5,99 @@
  */
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
-
+import '../../stylesheets/base.scss'
 
 class Register extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {first_name: '', last_name: '', username: '', password: '', email: '', phone: 0, address: '', number: 0, post_code: 0, country: '', birth_date: ''};
+        this.state = {ready: false, first_name: '', last_name: '', username: '', password: '', email: '', phone: 0, birth_date: ''};
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.checkPasswordMatch = this.checkPasswordMatch.bind(this);
     }
     
     handleSubmit () {
        const us = document.getElementById('username').value;
        const pass = document.getElementById('pwd').value;
+       const fname = document.getElementById('first_name').value;
+       const lname = document.getElementById('last_name').value;
+       const email = document.getElementById('email').value;
+       const bdate = document.getElementById('birth_date').value;
+       const number = document.getElementById('phone').value;
        
-       this.setState(() => ({ username: us, password: pass}));
+       this.setState(() => ({ first_name: fname, last_name: lname, username: us, password: pass, email: email, phone: number, birth_date: bdate}));        
+    }
+    
+    show(text) {
+        document.getElementById("message").innerHTML = text;
+        var popup = document.getElementById("userpop");
+        popup.classList.toggle("show");
+    }
+    
+    clear() {
+        document.getElementById("message").innerHTML = '';
+        var popup = document.getElementById("userpop");
+        popup.classList.toggle("none");
+    }
+    
+    checkPasswordMatch() {
+        var password = document.getElementById('pwd').value;
+        var confirmPassword = document.getElementById('re_pass').value;
         
+        if (password === confirmPassword) {
+            this.setState( () => ({ready: true}));
+            document.getElementById("message").innerHTML = "";
+        }
+        else {
+            this.setState( () => ({ready: false}));
+            document.getElementById("message").innerHTML = "Password must match";
+        }
     }
     render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-            <div>
-                </div>
+        return(
+            <form onSubmit={() => this.handleSubmit()}>
+                <div></div>
                 <div class="form-group">
                     <label for="first_name">First Name:</label>
-                    <input id="first_name" name="first_name" class="form-control" type="text" required/>
+                    <input id="first_name" name="first_name" class="form-control" pattern="[A-Za-z]+" type="text" required/>
                 </div>
                 
                 <div class="form-group">
                     <label for="last_name">Last Name:</label>
-                    <input id="last_name" name="last_name" class="form-control" type="text" required/>
+                    <input id="last_name" name="last_name" class="form-control" pattern="[A-Za-z]+" type="text" required/>
                 </div>
                 
                 <div class="form-group">
                     <label for="email">Email:</label>
-                    <input type="email" id="email" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" size="30" required/>
+                    <input type="email" id="email" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required/>
                 </div>
                 
                 <div class="form-group">
                     <label for="username">Username:</label>
-                    <input id="username" name="username" class="form-control" type="text" required/>
+                    <input id="username" name="username" class="form-control" title="only letters, numbers and underscore" pattern="[A-Za-z0-9_]+" type="text" required/>
                 </div>
-
+                
                 <div class="form-group">
                     <label for="pwd">Password:</label>
-                    <input type="password" name="password" class="form-control" id="pwd" required></input>
+                    <input title="no special characters" type="password" name="password" class="form-control" pattern="[A-Za-z0-9]{8,16}" id="pwd" onKeyUp={() => this.checkPasswordMatch()} required></input>
+                    
                 </div>
                 
                 <div class="form-group">
                     <label for="pwd_repeat">Repeat Password:</label>
-                    <input id="re_pass" name="re_pass" class="form-control" type="password" required/>
+                    <input id="re_pass" name="re_pass" class="form-control" type="password" onKeyUp={() => this.checkPasswordMatch()} required/>
+                    <span id='message'></span>
                 </div>
                 
                 <div class="form-group">
                     <label for="birth_date">Date of Birth:</label>
-                    <input type="date" id="birth_date" name="birth_date" class="form-control"  required/>
+                    <input type="date" id="birth_date" name="birth_date" class="form-control" max="2000-12-31" required/>
                 </div>
-                
-                <button type="submit" class="btn btn-default">Submit</button>
+                <div class="form-group">
+                    <label for="phone">Enter your phone number:</label>
+                    <input type="tel" id="phone" title="69XXXXXXXX" pattern="69\d{8}" name="phone" required/>
+                </div>
+               
+                <button type="submit" disabled={!this.state.ready} class="btn btn-default">Submit</button>
             </form>
         );
   }
