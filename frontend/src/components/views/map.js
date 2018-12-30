28d1f251798ddb87b13ec85b571723d1b6aab8de
nikolkats <nikolettakats@gmail.com>
2018-12-30 19:39:56 +0200

    map marker info, current location problem with sync

--- frontend/src/components/views/map.js
+++ frontend/src/components/views/map.js
@@ -4,34 +4,47 @@ import Geocode from 'react-geocode';
 import fetch from 'isomorphic-fetch';
 
 /* global google */
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-};
-
+    
 function coords_to_address (lat, long) {
-        fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + 
+    return fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + 
                 JSON.stringify(lat) + ',' + JSON.stringify(long) +
                 '&key=' + 'AIzaSyAsLsF3d7bdPcNMcSwPfb8aUfcadkjOMH0')
         .then((response) => response.json())
         .then((responseJson) => {
             if (responseJson.status === 'OK') {
-                alert(responseJson.results[0].formatted_address);
                 return responseJson.results[0].formatted_address;
             }
             else {
                 return 'not found';
             }
         });
-    }
-    
+}
+ 
+async function getLocation ()  {
+   const location = window.navigator && window.navigator.geolocation;
+
+   if (location) {
+       location.getCurrentPosition(async (position) => {
+           var lat = position.coords.latitude;
+           var long = position.coords.longitude;
+           let address = await coords_to_address(lat, long)
+                       .then((address) => {return address;});
+           console.log(address);
+           return [position.coords.latitude, position.coords.longitude, address];
+       }, (error) => {
+           console.log('error current location', error);
+       });
+   }
+}
+
 export class MapClass extends Component {
     constructor(props) {
         super(props);
-        this.getLocation = this.getLocation.bind(this);
+        this.currentLocation = this.currentLocation.bind(this);
         this.state = {current: [], show_current: false,
             markers : [], activeMarker: {},  showingInfoWindow: false};
         this.onMarkerClick = this.onMarkerClick.bind(this);
+        this.info = this.info.bind(this);
     }
     
     componentDidMount() {
@@ -39,23 +52,8 @@ export class MapClass extends Component {
         this.setState({markers : [{latitude: 37.9763, longitude:23.79763, id:0, price: 17, name: 'CAVA_0'}, 
                     {latitude: 37.9738, longitude:23.7275, id:1, price: 21, name: 'CAVA_1'}]});
     }
-
-    coords_to_address (lat, long) {
-        fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + 
-                JSON.stringify(lat) + ',' + JSON.stringify(long) +
-                '&key=' + 'AIzaSyAsLsF3d7bdPcNMcSwPfb8aUfcadkjOMH0')
-        .then((response) => response.json())
-        .then((responseJson) => {
-            if (responseJson.status === 'OK') {
-                return responseJson.results[0].formatted_address;
-            }
-            else {
-                return 'not found';
-            }
-        });
-    }
     
-    getLocation ()  {
+    async currentLocation ()  {
         var checkBox = document.getElementById("location");
         if (!checkBox.checked) {
             var temp = this.state.show_current;
@@ -63,24 +61,19 @@ export class MapClass extends Component {
             return;
         }
         
-        const location = window.navigator && window.navigator.geolocation;
+        let result = await getLocation();
+        console.log(result);
+        var temp = this.state.show_current;
+        this.setState({ current: [{latitude: result[0], longitude: result[1]}], show_current: !temp});
         
-        if (location) {
-            location.getCurrentPosition((position) => {
-                this.setState({
-                    current : [{latitude: position.coords.latitude, longitude: position.coords.longitude}]
-                });
-            }, (error) => {
-                console.log('error current location');
-            });
-            
-            var temp = this.state.show_current;
-            this.setState({ show_current: !temp});
-            
-            if (this.state.current.length > 0) {
-                var address = coords_to_address(this.state.current[0].latitude, this.state.current[0].longitude);
-                console.log(address);
-            }
+    }
+    
+    info (id) {
+        try {
+            return this.state.markers[id].name;
+        }
+        catch (error){
+            return;
         }
     }
     
@@ -96,7 +89,7 @@ export class MapClass extends Component {
         return (
             <div>
                 <label> Εμφάνιση Τωρινής Τοποθεσίας</label>
-                <input type="checkbox" id="location" name="location" onChange={() => this.getLocation()}></input>
+                <input type="checkbox" id="location" name="location" onChange={() => this.currentLocation()}></input>
                 <Map 
                     google={window.google}
                     zoom={11}
@@ -116,6 +109,7 @@ export class MapClass extends Component {
                         labelStyle={{color: '#fff'}}
                         icon ={'https://img.icons8.com/color/48/000000/speech-bubble.png'}
                         onClick={this.onMarkerClick}
+                        id={marker.id}
                     >
                     </Marker>
                     ))}
@@ -127,13 +121,13 @@ export class MapClass extends Component {
                             onClick={this.onMarkerClick}
                         >
                         </Marker>
-                        : <div/>
+                        : null
                         ))}
                         <InfoWindow
                             marker={this.state.activeMarker}
                             visible={this.state.showingInfoWindow}>
                             <div>
-                                <h1>this.state.activeMarker</h1>
+                            {this.info(this.state.activeMarker.id)}
                             </div>
                         </InfoWindow>
                 </Map>
