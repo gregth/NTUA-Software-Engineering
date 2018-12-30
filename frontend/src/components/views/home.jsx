b320490be1769fc87d0dd05ca74285b56d2da820
nikolkats <nikolettakats@gmail.com>
2018-12-30 15:48:44 +0200

    maps

--- frontend/src/components/views/home.jsx
+++ frontend/src/components/views/home.jsx
@@ -1,25 +1,24 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faHome } from '@fortawesome/free-solid-svg-icons';
-import { GeolocatedProps, geolocated } from 'react-geolocated';
 import { browserHistory } from 'react-router';
-import Slider from 'react-rangeslider';
-import Geocode from 'react-geocode';
+import MapClass from './map';
 import Range from './range';
+import {Product} from './product';
+import {Shop} from './shop';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
-        this.state = {search: [], latitude: '', longitude: '', price: 50};
+        this.state = {search: [], price: 50, show_map: false, products: [], results: false};
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.defaultProps = {center: {lat: 59.95, lng: 30.33}, zoom: 11};
-        this.getMyLocation = this.getMyLocation.bind(this);
-        this.toggleCheckbox = this.toggleCheckbox.bind(this);
-        this.homepage = this.homepage.bind(this);
+        this.only_nearby_shops = this.only_nearby_shops.bind(this);
         this.updateRange = this.updateRange.bind(this);
+        this.selectedCheckboxes = new Set();        
     }
     
-    updateRange(val) {
+    updateRange (val) {
         this.setState({
             price: val
         });
@@ -37,7 +36,9 @@ export default class Home extends Component {
       browserHistory.push('/');
     }
     
-    toggleCheckbox(label){
+    toggleCheckbox (label, event){
+        event.preventDefault();
+        event.nativeEvent.stopImmediatePropagation();
         if (this.selectedCheckboxes.has(label)) {
             this.selectedCheckboxes.delete(label);
         } 
@@ -46,39 +47,24 @@ export default class Home extends Component {
         }
     }
   
-    getMyLocation() {
-        const location = window.navigator && window.navigator.geolocation;
-
-        if (location) {
-            location.getCurrentPosition((position) => {
-                this.setState({
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude
-                });
-            }, (error) => {
-                this.setState({ latitude: 'err-latitude', longitude: 'err-longitude' });
-            });
-        }
-        
-        Geocode.fromLatLng("48.8583701", "2.2922926").then(
-            response => {
-                const address = response.results[0].formatted_address;
-                console.log(address);
-            },
-            error => {
-                alert(error);
-            }
-        );
+    only_nearby_shops () {
+        //TODO send request
     }
     
-    handleSubmit () {
+    handleSubmit (event) {
+        event.preventDefault();
+        event.nativeEvent.stopImmediatePropagation();
         const s = document.getElementById('search').value;
        
         if (s !== '') {
             this.selectedCheckboxes.add(s);
         }
+        var temp = this.state.show_map;
         
-        this.setState(() => ({search: this.selectedCheckboxes}));
+        var shop = new Shop({name: 'cava1', id: 1, address: 'Athens 1', lat: 37.9738, lgn:23.7275});
+        var product = new Product({name: 'pr1', barcode: 12345, price: 12, shop:shop, favourite: true, id: 0});
+        this.state.products.push(product);
+        this.setState({results: true, show_map: !temp, search: this.selectedCheckboxes});
     }
     
     Login() {
@@ -89,50 +75,59 @@ export default class Home extends Component {
         browserHistory.push('/register');
     }
     
-    Map() {
-        browserHistory.push('/map');
-    }
-    
     render() {
       return (
         <div> 
             <button className="btn" type="submit" id="button1" onClick={() => this.Login()}>Σύνδεση</button>
             <button className="btn" type="submit" id="button2" onClick={() => this.Register()}>Εγγραφή</button>
             <div className="search">
-                <h1> Αναζήτηση Προϊόντων </h1>
-                <form id="searching" onSubmit={() => this.handleSubmit()}>
-                    <label> Κρασί </label>
-                    <input type="checkbox" name="product" value="wine" onChange={() => this.toggleCheckbox("Κρασί")}></input>
-                    <label> Μπύρες </label>
-                    <input type="checkbox" name="product" value="beer" onChange={() => this.toggleCheckbox("Μπύρες")}></input>
-                    <label> Βότκα </label>
-                    <input type="checkbox" name="product" value="vodka" onChange={() => this.toggleCheckbox("Βότκα")}></input>
-                    <label> Ουίσκι </label>
-                    <input type="checkbox" name="product" value="whiskey" onChange={() => this.toggleCheckbox("Ουίσκι")}></input>
-                    <label> Ρούμι </label>
-                    <input type="checkbox" name="product" value="Rum" onChange={() => this.toggleCheckbox("Ρούμι")}></input>
-                    <br/><br/>
-                    <label> Gin </label>
-                    <input type="checkbox" name="product" value="Gin" onChange={() => this.toggleCheckbox("Gin")}></input>
-                    <label> Τεκίλα </label>
-                    <input type="checkbox" name="product" value="Tequila" onChange={() => this.toggleCheckbox("Τεκίλα")}></input>
-                    <label> Αναψυκτικά </label>
-                    <input type="checkbox" name="product" value="beverages" onChange={() => this.toggleCheckbox("Αναψυκτικά")}></input>
-                    <label> Snacks </label>
-                    <input type="checkbox" name="product" value="snancks" onChange={() => this.toggleCheckbox("Snacks")}></input>
-                    <label> Χωρίς Αλκοόλ </label>
-                    <input type="checkbox" name="product" value="nonalchool" onChange={() => this.toggleCheckbox("Χωρίς Αλκοόλ")}></input>
-                    <br/>
-                    <input id="search" type="text" placeholder="Search.." name="search"></input>
-                    <button className="search_btn" id="search_btn" type="submit"><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></button>
-                    <br/><br/>
-                    <label> Μέγιστη τιμή </label>
-                    <Range range={this.state.price} updateRange={this.updateRange}/>
-                    <br/>
-                    <label> Only nearby shops</label>
-                    <input type="checkbox" name="location" onChange={() => this.getMyLocation()}></input>
-                </form>
-            </div>
+                    <h1> Αναζήτηση Προϊόντων </h1>
+                    <form id="searching">
+                        <label> Κρασί </label>
+                        <input type="checkbox" name="product" value="wine" onChange={() => this.toggleCheckbox("Κρασί")}></input>
+                        <label> Μπύρες </label>
+                        <input type="checkbox" name="product" value="beer" onChange={() => this.toggleCheckbox("Μπύρες")}></input>
+                        <label> Βότκα </label>
+                        <input type="checkbox" name="product" value="vodka" onChange={() => this.toggleCheckbox("Βότκα")}></input>
+                        <label> Ουίσκι </label>
+                        <input type="checkbox" name="product" value="whiskey" onChange={() => this.toggleCheckbox("Ουίσκι")}></input>
+                        <label> Ρούμι </label>
+                        <input type="checkbox" name="product" value="Rum" onChange={() => this.toggleCheckbox("Ρούμι")}></input>
+                        <br/><br/>
+                        <label> Gin </label>
+                        <input type="checkbox" name="product" value="Gin" onChange={() => this.toggleCheckbox("Gin")}></input>
+                        <label> Τεκίλα </label>
+                        <input type="checkbox" name="product" value="Tequila" onChange={() => this.toggleCheckbox("Τεκίλα")}></input>
+                        <label> Αναψυκτικά </label>
+                        <input type="checkbox" name="product" value="beverages" onChange={() => this.toggleCheckbox("Αναψυκτικά")}></input>
+                        <label> Snacks </label>
+                        <input type="checkbox" name="product" value="snancks" onChange={() => this.toggleCheckbox("Snacks")}></input>
+                        <label> Χωρίς Αλκοόλ </label>
+                        <input type="checkbox" name="product" value="nonalchool" onChange={() => this.toggleCheckbox("Χωρίς Αλκοόλ")}></input>
+                        <br/>
+                        <input id="search" type="text" placeholder="Search.." name="search"></input>
+                        <button className="search_btn" id="search_btn" type="submit" onClick={this.handleSubmit}><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></button>
+                        <br/><br/>
+                        <label> Μέγιστη τιμή </label>
+                        <Range range={this.state.price} updateRange={this.updateRange}/>
+                        <br/>
+                        <label> Only nearby shops</label>
+                        <input type="checkbox" name="location" onChange={() => this.only_nearby_shops()}></input>
+                        
+                        {this.state.results
+                            ? <div> {this.state.products.map(product => (
+                                    <div> {product.name} {product.price}€ 
+                                    </div>
+                                ))}</div>
+                            : <div></div>
+                        }
+                        {this.state.show_map
+                            ?<MapClass/>
+                            : <div/>
+                        }   
+                    </form>
+                    
+                </div>
         </div>
 
           );
--- frontend/src/components/views/map.js
+++ frontend/src/components/views/map.js
@@ -3,23 +3,56 @@ import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import Geocode from 'react-geocode';
 import fetch from 'isomorphic-fetch';
 
+/* global google */
 const mapStyles = {
   width: '100%',
   height: '100%'
 };
 
+function coords_to_address (lat, long) {
+        fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + 
+                JSON.stringify(lat) + ',' + JSON.stringify(long) +
+                '&key=' + 'AIzaSyAsLsF3d7bdPcNMcSwPfb8aUfcadkjOMH0')
+        .then((response) => response.json())
+        .then((responseJson) => {
+            if (responseJson.status === 'OK') {
+                alert(responseJson.results[0].formatted_address);
+                return responseJson.results[0].formatted_address;
+            }
+            else {
+                return 'not found';
+            }
+        });
+    }
+    
 export class MapClass extends Component {
     constructor(props) {
         super(props);
-        this.state = {current: [], show_current: false,
-            markers : []};
         this.getLocation = this.getLocation.bind(this);
+        this.state = {current: [], show_current: false,
+            markers : [], activeMarker: {},  showingInfoWindow: false};
+        this.onMarkerClick = this.onMarkerClick.bind(this);
     }
     
     componentDidMount() {
         //TODO request results
         this.setState({markers : [{latitude: 37.9763, longitude:23.79763, id:0, price: 17, name: 'CAVA_0'}, 
                     {latitude: 37.9738, longitude:23.7275, id:1, price: 21, name: 'CAVA_1'}]});
+    }
+
+    coords_to_address (lat, long) {
+        fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + 
+                JSON.stringify(lat) + ',' + JSON.stringify(long) +
+                '&key=' + 'AIzaSyAsLsF3d7bdPcNMcSwPfb8aUfcadkjOMH0')
+        .then((response) => response.json())
+        .then((responseJson) => {
+            if (responseJson.status === 'OK') {
+                return responseJson.results[0].formatted_address;
+            }
+            else {
+                return 'not found';
+            }
+        });
     }
     
     getLocation ()  {
@@ -45,19 +78,20 @@ export class MapClass extends Component {
             this.setState({ show_current: !temp});
             
             if (this.state.current.length > 0) {
-                fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + 
-                        JSON.stringify(this.state.current[0].latitude) + ',' + JSON.stringify(this.state.current[0].longitude) +
-                        '&key=' + 'AIzaSyAsLsF3d7bdPcNMcSwPfb8aUfcadkjOMH0')
-                .then((response) => response.json())
-                .then((responseJson) => {
-                    if (responseJson.status === 'OK') {
-                        console.log(responseJson.results[0].formatted_address);
-                    }
-                });
+                var address = coords_to_address(this.state.current[0].latitude, this.state.current[0].longitude);
+                console.log(address);
             }
         }
     }
     
+    onMarkerClick (props, marker, e) {
+        this.setState({
+          selectedPlace: props,
+          activeMarker: marker,
+          showingInfoWindow: true
+        });
+    }
+  
     render() {
         return (
             <div>
@@ -78,21 +112,30 @@ export class MapClass extends Component {
                         position={{ lat: marker.latitude, lng: marker.longitude }}
                         key={marker.id}
                         label={marker.price.toString() + '€'}
-                        onClick={() => this.info(marker.id)}
+                        onClick={this.info}
                         labelStyle={{color: '#fff'}}
                         icon ={'https://img.icons8.com/color/48/000000/speech-bubble.png'}
-                    />
+                        onClick={this.onMarkerClick}
+                    >
+                    </Marker>
                     ))}
                     {this.state.current.map(marker => (
                         this.state.show_current
                         ? <Marker
                             position={{ lat: marker.latitude, lng: marker.longitude }}
-                            key={0} icon={'https://www.robotwoods.com/dev/misc/bluecircle.png'}
+                            key={3} icon={'https://www.robotwoods.com/dev/misc/bluecircle.png'}
+                            onClick={this.onMarkerClick}
                         >
                         </Marker>
                         : <div/>
                         ))}
-                    
+                        <InfoWindow
+                            marker={this.state.activeMarker}
+                            visible={this.state.showingInfoWindow}>
+                            <div>
+                                <h1>this.state.activeMarker</h1>
+                            </div>
+                        </InfoWindow>
                 </Map>
             </div>
             
