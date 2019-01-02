fdfa00f8b3113dd608a01cdf74b938e257a10221
Themis Papameletiou <themicp@gmail.com>
2019-01-02 20:07:16 +0200

    [BASE_CONTROLLER]: Pass only the params in validators instead of the whole request objects

--- backend/controllers/base.js
+++ backend/controllers/base.js
@@ -13,16 +13,16 @@ module.exports = class BaseController {
         this.model = model
     }
     
-    validate_post_request(req, res) {
+    validate_post_params(params) {
         let rules = this.model.rules.insert
-        let required_params = filter_keys(req.body, rules.required_fields);
+        let required_params = filter_keys(params, rules.required_fields);
         if (Object.keys(required_params).length != rules.required_fields.length) {
             console.log("[POST_VALIDATOR]: Not all required post params provided")
             throw new Error('Not all required post params provided')
         }
 
         let validated_params = required_params
-        let optional_params = filter_keys(req.body, rules.optional_fields);
+        let optional_params = filter_keys(params, rules.optional_fields);
         for (var k in optional_params)  {
            validated_params[k]  = optional_params[k]
         }
@@ -30,10 +30,10 @@ module.exports = class BaseController {
         return validated_params
     }
 
-    validate_put_request(req, res, id) {
+    validate_put_params(params) {
         let rules = this.model.rules.update
         console.log(rules)
-        let required_params = filter_keys(req.body, rules.updatable_fields);
+        let required_params = filter_keys(params, rules.updatable_fields);
         console.log(required_params	)
         // For a put request to take place, all updateable fields must be passed
         if (Object.keys(required_params).length != rules.updatable_fields.length) {
@@ -44,9 +44,9 @@ module.exports = class BaseController {
         return required_params
     }
 
-    validate_patch_request(req, res, id) {
+    validate_patch_params(params) {
         let rules = this.model.rules.update
-        let required_params = filter_keys(req.body, rules.updatable_fields);
+        let required_params = filter_keys(params, rules.updatable_fields);
         // For a put request to take place, all updateable fields must be passed
         if (Object.keys(required_params).length == 0) {
             console.log('[PATCH_VALIDATOR]: No updatable attributes provided in params')
