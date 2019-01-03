5d93e5024777dea49a465aea18402987e402ae2a
atsta <ath.ctam@gmail.com>
2019-01-03 17:48:18 +0200

    front

--- frontend/src/components/views/categories_menu.js
+++ frontend/src/components/views/categories_menu.js
@@ -3,20 +3,25 @@ import SelectSearch from 'react-select-search';
 
 const options = [
     {name: 'Όλες οι κατηγορίες', value: 'all'},
-    {name: 'Αναψυκτικά', value: 'beverages'},
-    {name: 'Βότκα', value: 'vodka'},
-    {name: 'Κονιάκ', value: 'koniak'},
-    {name: 'Κρασί', value: 'wine'},
-    {name: 'Λικέρ', value: 'liquer'},
-    {name: 'Μπύρες', value: 'beer'},
-    {name: 'Ούζο', value: 'ouzo'},
-    {name: 'Ουίσκι', value: 'whiskey'},
-    {name: 'Ρούμι', value: 'rum'},
-    {name: 'Σνακς', value: 'snacks'},
-    {name: 'Τζιν', value: 'gin'},
-    {name: 'Τεκίλα', value: 'tequila'},
-    {name: 'Τσίπουρο', value: 'tsipouro'},
-    {name: 'Χωρίς Αλκοόλ', value: 'nonalchool'}
+    {
+        type: 'group',
+        items: [
+            {name: 'Αναψυκτικά', value: 'beverages'},
+            {name: 'Βότκα', value: 'vodka'},
+            {name: 'Κονιάκ', value: 'koniak'},
+            {name: 'Κρασί', value: 'wine'},
+            {name: 'Λικέρ', value: 'liquer'},
+            {name: 'Μπύρες', value: 'beer'},
+            {name: 'Ούζο', value: 'ouzo'},
+            {name: 'Ουίσκι', value: 'whiskey'},
+            {name: 'Ρούμι', value: 'rum'},
+            {name: 'Σνακς', value: 'snacks'},
+            {name: 'Τζιν', value: 'gin'},
+            {name: 'Τεκίλα', value: 'tequila'},
+            {name: 'Τσίπουρο', value: 'tsipouro'},
+            {name: 'Χωρίς Αλκοόλ', value: 'nonalchool'}
+        ].sort(compare)
+    }
 ];
 
 function compare(a, b){
@@ -29,7 +34,7 @@ export class Categories extends Component {
   render() {
         return ( 
             <div>
-                <SelectSearch options={options.sort(compare)} value="all" name="categories" placeholder="Επιλογή Κατηγορίας" />
+                <SelectSearch options={options} value="all" name="categories" placeholder="Επιλογή Κατηγορίας" />
             </div>
         );
     }
