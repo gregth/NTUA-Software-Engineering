ff5893ea910a954a5f6512e02f03bf707477621d
Panagiotis Kostopanagiotis <panagiotis.kostopanagiotis@gmail.com>
2019-01-11 23:26:00 +0200

    Customized the map

--- testApp/components/MapPage.js
+++ testApp/components/MapPage.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
+import { GeolocatedProps, geolocated } from 'react-geolocated';
+
+export const getCurrentLocation = () => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e));
+  });
+};
 
 class MapPage extends Component{
+  componentDidMount() {
+    return getCurrentLocation().then(position => {
+      if (position) {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.003,
+            longitudeDelta: 0.003,
+          },
+        });
+      }
+    }
+    )
+  }
   static navigationOptions = {
     title: 'Τωρινή τοποθεσία',
   };
@@ -10,8 +32,7 @@ class MapPage extends Component{
     return (
         <MapView
          style = {styles.map}
-         showsUserLocation = {false}
-         followUserLocation = {false}
+         showsUserLocation = {true} 
          zoomEnabled = {true}
       />
     );
