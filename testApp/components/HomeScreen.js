66950f73ee9670ad1f79d45688fd4968d7bf2750
MarilenaOik <marilenaoikonomopoulou@gmail.com>
2019-01-14 15:26:46 +0200

    aaa

--- testApp/components/HomeScreen.js
+++ testApp/components/HomeScreen.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react';
-import { Button, View, ScrollView, Text } from 'react-native';
+import { Button, View, ScrollView, Text, Alert } from 'react-native';
 import { CheckBox } from 'react-native-elements'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Dropdown } from 'react-native-material-dropdown';
-import { SearchBar } from 'react-native-elements'
-import { Slider } from 'react-native-elements'
-//import {Navbar, Nav, NavItem, MenuItem, NavDropdown} from 'react-boostrap';
-//import { library } from '@fortawesome/fontawesome-svg-core'
-//import { faSearch, faHome } from '@fortawesome/free-solid-svg-icons';
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { SearchBar, Overlay, Slider, ButtonGroup, Tile } from 'react-native-elements'
 import { GeolocatedProps, geolocated } from 'react-geolocated';
-//import { browserHistory } from 'react-router';
-//import Slider from 'react-rangeslider';
 import Geocode from 'react-geocode';
-//import MapClass from './map';
 
 class HomeScreen extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+    text:'',
+    price: 0,
+    isVisible:true,
+    selectedIndex:-1};
+    this.updateIndex = this.updateIndex.bind(this)
+    this.handleLogIn = this.handleLogIn.bind(this)
+  }
+  updateIndex (selectedIndex) {
+    this.setState({selectedIndex: selectedIndex})
+  }
+  handleLogIn(selectedIndex) {
+    this.setState({selectedIndex:selectedIndex})
+    if(selectedIndex==0){
+      this.props.navigation.navigate('Login')
+    }
+    else if(selectedIndex==1){
+      this.props.navigation.navigate('Register')
+    }
+    else{
+      Alert.alert('You tapped the button!')
+    }
+  }
+
   static navigationOptions = {
     title: 'Home',
   };
+
+  render(){
+    return(
+      <Overlay> //isVisible={this.state.isVisible}>
+      <Text>Hello from Overlay!</Text>
+      </Overlay>
+
+    );
+  }
+
   render() {
     let data = [{
       value: 'Κρασί',
@@ -42,47 +70,54 @@ class HomeScreen extends Component{
       value: 'Χωρίς Αλκοόλ',
     }];
 
-    return ( <View>
-       <Text>Alcowatch</Text>
-      <Button
-          title="Είσοδος"
-          onPress={() => this.props.navigation.navigate('Login')}
+    const buttons = ['Σύνδεση', 'Εγγραφή']
+    const { selectedIndex } = this.state
+
+
+    return (
+      <View style={{flex: 2, marginHorizontal:15}}>
+      <ButtonGroup style={{right:40}}
+        onPress={this.handleLogIn}
+        selectedIndex={selectedIndex}
+        buttons={buttons}
+        containerStyle={{height: 20, width:150, left:175}}
         />
-      <Button
-          title="Δημιουργία Λογαριασμού"
-          onPress={() => this.props.navigation.navigate('Register')}
-        />
-        <Dropdown
-        label='Επιλογή Κατηγορίας'
+
+
+      // <Tile
+      //   imageSrc={require('./logo_transparent.png')}
+      //   containerStyle={{width:500, height:200}}
+      //   />
+
+      <Text style={{textAlign:'center'}}>Alcowatch</Text>
+
+      <Dropdown
+        label ='Επιλογή Κατηγορίας'
         data={data}
-      />
-      <SearchBar
-      lightTheme
-  placeholder='Αναζήτηση με όνομα...' />
-
-
-  <Slider
-    />
-<Text> Μέγιστη Τιμή</Text>
-        <Button
-            title="Εμφάνιση τοποθεσίας"
-            onPress={() =>this.props.navigation.navigate('Map')}
         />
-        </View>
+
+      <SearchBar
+        lightTheme
+        onChangeText={(text) => this.setState({text : text})}
+        onClear={(text) => this.setState({text : ''})}
+        placeholder='Αναζήτηση με όνομα...'
+        />
+
+      <Slider
+        value={this.state.price}
+        maximumValue = {value = 150}
+        onValueChange={(value) => this.setState({price : value})}
+        />
+
+      <Text> Μέγιστη Τιμή:{this.state.price.toFixed(2)}€</Text>
+
+      <Button
+        title="Εμφάνιση τοποθεσίας"
+        onPress={() =>this.props.navigation.navigate('Map')}
+        />
+
+      </View>
     );
   }
-/*      <React.Fragment>
-      <h1> Αναζήτηση Προϊόντων </h1>
-
-                          <input id="search" type="text" placeholder="Search.." name="search"></input>
-                          <button id="search_btn" type="submit">
-                          <br/><br/>
-                          <label> Μέγιστη τιμή </label>
-                          <Range range={this.state.price} updateRange={this.updateRange}/>
-                          <br/>
-                          <label> Only nearby shops</label>
-      </React.Fragment>
-*/
-
 }
 export default HomeScreen;
