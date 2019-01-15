417303f444b0a973e51d725b10e267af6ce4d429
nikolkats <nikolettakats@gmail.com>
2019-01-15 18:27:24 +0200

    all products papigation

--- frontend/src/components/helper_components/all_products_papigation.js
+++ frontend/src/components/helper_components/all_products_papigation.js
@@ -9,107 +9,143 @@ import React, { Component } from "react";
 import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import {receive_from_server} from '../communication/receive';
 
 export default class PapigationResults extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.toggle = this.toggle.bind(this);
-    this.createData = this.createData.bind(this);
-    this.state = {
-        tooltipOpen: false, currentPage: 0
-    };
-    this.dataSet = null;    
-    this.pagesCount = null;
-    this.pageSize = 10;
+        this.toggle = this.toggle.bind(this);
+        this.createData = this.createData.bind(this);
+        this.request = this.request.bind(this);
+        this.state = {
+            tooltipOpen: false, currentPage: 0, error: null, success: null, not_found: null, ready: null
+        };
+        this.dataSet = null;    
+        this.pagesCount = null;
+        this.pageSize = 3;
+        this.sort = 'id|DESC';
+        this.status = 'ACTIVE';
+        this.start = 0;
+        this.total = null;
+        this.products = null;
+        this.request();
   }
   
-  createData () {
-      this.dataSet = this.props.data.map(product => (
-        <tr key={product.id} onClick={() => this.props.select(product.id)} className="row_pointer">
-            <td>{product.name}</td>
-            <td>{product.category}</td>
-            <td>
-                <TooltipItem id={product.id} text={product.description}/>
-            </td>
-            <td>Brand</td>
-            <td>
-                <button className="search_btn" id="edit_btn" onClick={() => this.props.edit(product.id)}><FontAwesomeIcon icon={faEdit}></FontAwesomeIcon></button>
-                <button className="search_btn" id="delete_btn" onClick={() => this.props.delete(product.id, product.name)}><FontAwesomeIcon icon={faTrashAlt}></FontAwesomeIcon></button>     
-                <button className="search_btn" id="search_product_btn" onClick={() => this.props.search(product.id)}><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></button>
-            </td>
-        </tr>
-    ));
-    this.pagesCount = Math.ceil(this.dataSet.length / this.pageSize);  
-  }
-  
-  toggle() {
+    createData () {
+        this.dataSet = this.products.map(product => (
+            <tr key={product.id} onClick={() => this.props.select(product.id)} className="row_pointer">
+                <td>{product.name}</td>
+                <td>{product.category}</td>
+                <td>
+                    <TooltipItem id={product.id} text={product.description}/>
+                </td>
+                <td>Brand</td>
+                <td>
+                    <button className="search_btn" id="edit_btn" onClick={() => this.props.edit(product.id)}><FontAwesomeIcon icon={faEdit}></FontAwesomeIcon></button>
+                    <button className="search_btn" id="delete_btn" onClick={() => this.props.delete(product.id, product.name)}><FontAwesomeIcon icon={faTrashAlt}></FontAwesomeIcon></button>     
+                    <button className="search_btn" id="search_product_btn" onClick={() => this.props.search(product.id)}><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></button>
+                </td>
+            </tr>
+        ));
+    } 
+    
+    toggle() {
         this.setState({
             tooltipOpen: !this.state.tooltipOpen
         });
     }
     
-  handleClick(e, index) {
+    async request () {        
+        const url = 'http://localhost:3002/products?start=' + this.start + 
+                    '&count=' +  this.pageSize + '&sort=' + this.sort +
+                    '&status=' + this.status;
+        const answer = await receive_from_server(url);
+        
+        if (answer === 'error') {
+            this.setState({error: true});
+            return;
+        }
+        
+        if (answer.status === 200) {
+            this.setState({success: true});
+        }
+        else {
+            this.setState({not_found: true});
+        }
+        var result = await answer.json().then((result) => {return result;});
+        console.log(result);
+        if (this.start !== result.start || this.pageSize !== result.count) {
+            this.setState({not_found: true, success: false});
+            return;
+        }
+        this.total = result.total;
+        this.products = result.products;
+        this.pagesCount = Math.ceil(this.total / this.pageSize);
+        this.createData();
+        this.setState({ready: true});
+    }
     
-    e.preventDefault();
+    async handleClick(e, index) {
+        e.preventDefault();
+        this.start = index*this.pageSize;
+        this._isMounted = await this.request();
+        
+        this.setState({
+            currentPage: index
+        });
+    }
 
-    this.setState({
-      currentPage: index
-    });
-    
-  }
+    render() {
+        const { currentPage } = this.state;
+        return ( 
+            <div>
+                {!this.state.ready
+                ?<div> Loading </div>
+                :<React.Fragment>   
+                    <Table hover>
+                        <thead>
+                            <tr>
+                                <th>Όνομα Προϊόντος</th>
+                                <th>Κατηγορία</th>
+                                <th>Περιγραφή</th>
+                                <th>Μάρκα</th>
+                            </tr>
+                        </thead>
+                        {this.dataSet.map((data, i) => 
+                        <tbody className="data-slice" key={i}>
+                            {data}
+                        </tbody>
 
-  render() {
-    const { currentPage } = this.state;
-    this.createData();
-    return (    
-        <React.Fragment>   
-            <Table hover>
-                <thead>
-                    <tr>
-                        <th>Όνομα Προϊόντος</th>
-                        <th>Κατηγορία</th>
-                        <th>Περιγραφή</th>
-                        <th>Μάρκα</th>
-                    </tr>
-                </thead>
-                {this.dataSet
-                .slice(
-                    currentPage * this.pageSize,
-                    (currentPage + 1) * this.pageSize
-                )
-                .map((data, i) => 
-                <tbody className="data-slice" key={i}>
-                    {data}
-                </tbody>
-
-                )}
-            </Table>
-            <div className="pagination-wrapper">          
-                <Pagination aria-label="Page navigation example">            
-                    <PaginationItem disabled={currentPage <= 0}>              
-                        <PaginationLink
-                            onClick={e => this.handleClick(e, currentPage - 1)}
-                            previous
-                            href="#"
-                        />              
-                    </PaginationItem>
-                    {[...Array(this.pagesCount)].map((page, i) => 
-                        <PaginationItem active={i === currentPage} key={i}>
-                            <PaginationLink onClick={e => this.handleClick(e, i)} href="#">
-                                {i + 1}
-                            </PaginationLink>
-                        </PaginationItem>
-                    )}
-                    <PaginationItem disabled={currentPage >= this.pagesCount - 1}>
-                        <PaginationLink
-                            onClick={e => this.handleClick(e, currentPage + 1)}
-                            next
-                            href="#"
-                        />
-                    </PaginationItem>
-                </Pagination>
+                        )}
+                    </Table>
+                    <div className="pagination-wrapper">          
+                        <Pagination aria-label="Page navigation example">            
+                            <PaginationItem disabled={currentPage <= 0}>              
+                                <PaginationLink
+                                    onClick={e => this.handleClick(e, currentPage - 1)}
+                                    previous
+                                    href="#"
+                                />              
+                            </PaginationItem>
+                            {[...Array(this.pagesCount)].map((page, i) => 
+                                <PaginationItem active={i === currentPage} key={i}>
+                                    <PaginationLink onClick={e => this.handleClick(e, i)} href="#">
+                                        {i + 1}
+                                    </PaginationLink>
+                                </PaginationItem>
+                            )}
+                            <PaginationItem disabled={currentPage >= this.pagesCount - 1}>
+                                <PaginationLink
+                                    onClick={e => this.handleClick(e, currentPage + 1)}
+                                    next
+                                    href="#"
+                                />
+                            </PaginationItem>
+                        </Pagination>
+                    </div>
+                </React.Fragment>
+            }
             </div>
-    </React.Fragment>
     
     );
   
--- frontend/src/components/views/all_products.js
+++ frontend/src/components/views/all_products.js
@@ -5,7 +5,6 @@
  */
 
 import React, { Component } from "react";
-import {receive_from_server} from '../communication/receive';
 import { browserHistory } from 'react-router';
 import cookie from 'react-cookies';
 import {Settings} from '../helper_components/dropdown_settings';
@@ -19,7 +18,6 @@ export class AllProducts extends Component {
         super(props);
         this.id = null;
         this.select = this.select.bind(this);
-        this.request = this.request.bind(this);
         this.state = {products: null, error: null, success: null, not_found: null};
         this.homepage = this.homepage.bind(this);
         this.search = this.search.bind(this);
@@ -39,51 +37,12 @@ export class AllProducts extends Component {
         catch(error) {
             console.log(error);
         }
-        
-        this._asyncRequest = this.request().then(
-            products => {
-                this._asyncRequest = null;
-                this.setState({products});
-            }
-        );
     }
     
-    componentWilldUnmount() {
-        if (this._asyncRequest) {
-            this._asyncRequest.cancel();
-        }
-    }
-
-    async request() {
-        const url = 'http://localhost:3002/products';
-        const answer = await receive_from_server(url);
-        
-        if (answer === 'error') {
-            this.setState({error: true});
-            return;
-        }
-        
-        if (answer.status === 200) {
-            this.setState({success: true});
-        }
-        else {
-            this.setState({not_found: true});
-        }
-        
-        var products = await answer.json().then((result) => {return result.products;});
-        console.log(products);
-        this.setState({products: products});
-        return products;
-    }
-    
-    async delete_complete () {
-        this._asyncRequest = await this.request().then(
-            shops => {
-                this._asyncRequest = null;
-                this.setState({shops});
-            }
-        );
+    delete_complete () {
+        this.setState({products: false});
         this.refs.delete.closeall();
+        this.setState({products: true});
     }
     
     edit (id) {
@@ -111,7 +70,7 @@ export class AllProducts extends Component {
     }
     
     homepage() {
-        browserHistory.push('/search');
+        browserHistory.push('/');
     }
     
     render() {
@@ -119,10 +78,7 @@ export class AllProducts extends Component {
             <div>
                 <NavBarClass/>
                 <Alert color="danger" isOpen={this.state.error===true}>Πρόβλημα με τη σύνδεση. Δοκιμάστε ξανά.</Alert>
-                {this.state.products === null
-                ?<div> Loading </div>
-                : <PapigationResults data={this.state.products} select={this.select} delete={this.delete} edit={this.edit} search={this.search}/>
-                }
+                <PapigationResults select={this.select} delete={this.delete} edit={this.edit} search={this.search}/>
                 <Delete ref='delete' back={this.delete_complete} category="product" id={this.id} name={this.name}/>
             </div>
         );
