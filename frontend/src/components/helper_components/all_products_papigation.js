21b5094b0eb8d5b9c8d1f946550d7c4530d94e6d
nikolkats <nikolettakats@gmail.com>
2019-02-18 12:21:31 +0100

    error message all products

--- frontend/src/components/helper_components/all_products_papigation.js
+++ frontend/src/components/helper_components/all_products_papigation.js
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 import Description from './product_description';
-import { Input, Table, Pagination, PaginationItem, PaginationLink, Tooltip, Button } from 'reactstrap';
+import { Input, Table, Pagination, PaginationItem, PaginationLink, Tooltip, Button, Alert } from 'reactstrap';
 import React, { Component } from "react";
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
@@ -27,9 +27,10 @@ export default class ProductsResults extends React.PureComponent {
         this.search = this.search.bind(this);
         this.search_product = this.search_product.bind(this);
         this.state = {
-            tooltipOpen: false, currentPage: 0, error: null, success: null, not_found: null, ready: null
+            tooltipOpen: false, currentPage: 0, error: null, success: null, not_found: null, ready: null, message: null
         };
         this._asyncRequest = null;
+        this._isMounted = null;
         this.dataSet = null;    
         this.pagesCount = null;
         this.pageSize = 20;
@@ -45,6 +46,9 @@ export default class ProductsResults extends React.PureComponent {
     componentWilldUnmount() {
         if (this._asyncRequest) {
             this._asyncRequest.cancel();
+        }
+        if (this._isMounted) {
+            this._isMounted.cancel();
         }
     }
     
@@ -121,21 +125,45 @@ export default class ProductsResults extends React.PureComponent {
         this._asyncRequest = await receive_from_server(url);
         const answer = this._asyncRequest;
         
-        if (answer === 'error') {
-            this.setState({error: true});
+        try {
+            if (answer === 'error') {
+                this.setState({error: true});
+                return;
+            }
+
+            if (answer.status === 200) {
+                this.setState({success: true});
+            }
+            else if (answer.status === 404) {
+                this.setState({message: 'Error 404 - Το αίτημα δεν ήταν επιτυχές', not_found: true});
+                return;
+            }
+            else if (answer.status === 401) {
+                this.setState({message: 'Error 401 - Μη επιτρεπόμενη ενέργεια', not_found: true});
+                return;
+            }
+            else if (answer.status === 403) {
+                this.setState({message: 'Error 403 - Απαιτείται σύνδεση', not_found: true});
+                return;
+            }
+            else if (answer.status === 400) {
+                this.setState({message: 'Error 400 - Μη έγκυρες παράμετροι αιτήματος.', not_found: true});
+                return;
+            }
+            else {
+                this.setState({message: 'Error ' + answer.status.toString() + ' - Πρόβλημα με την ολοκλήρωση του αιτήματος.', not_found: true});
+                return;
+            }
+        }
+        catch (error) {
+            this.setState({error: true, error_message: error});
             return;
         }
         
-        if (answer.status === 200) {
-            this.setState({success: true});
-        }
-        else {
-            this.setState({not_found: true});
-        }
         var result = await answer.json().then((result) => {return result;});
         console.log(result);
         if (this.start !== result.start || parseInt(this.pageSize) !== result.count) {
-            this.setState({not_found: true, success: false});
+            this.setState({error: true, success: false});
             return;
         }
         
@@ -178,6 +206,10 @@ export default class ProductsResults extends React.PureComponent {
         const { currentPage } = this.state;
         return ( 
             <div>
+    
+                <Alert color="danger" isOpen={this.state.error===true}>Πρόβλημα με τη σύνδεση. Δοκιμάστε ξανά. {this.state.error_message}</Alert>
+                <Alert color="danger" isOpen={this.state.not_found===true}>{this.state.message}</Alert>
+                
                 <Table borderless>
                     <thead>
                         <tr>
