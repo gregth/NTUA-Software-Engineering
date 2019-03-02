71ded7631943ee2cfc1342ec387668a4766e6d32
Athina <ath.ctam@gmail.com>
2019-03-02 14:45:00 +0200

    aboutus page

--- frontend/src/components/views/aboutus.js
+++ frontend/src/components/views/aboutus.js
@@ -7,7 +7,7 @@
 import React, {Component} from 'react';
 import { browserHistory } from 'react-router';
 import { Alert, Row, Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Container, Col} from 'reactstrap';
+  CardTitle, CardSubtitle, Container, Col, CardDeck} from 'reactstrap';
 import NavBarClass from '../helper_components/navbar';
 
 class About extends Component {
@@ -23,50 +23,48 @@ class About extends Component {
                 <Alert color="danger" isOpen={this.state.error===true}>Πρόβλημα με τη σύνδεση. Δοκιμάστε ξανά.</Alert>
                 <div>
                     <Container className="About">
-                    <Row>
-                    <Col xs={6} md={4}>
+                    <CardDeck>
                         <Card>
-                            <CardImg top width="20%" src={"/public/athina.jpg"} alt="Card image cap" />
+                            <CardImg  width="200" height="260" src={"/public/athina.jpg"} alt="Card image cap" />
                             <CardBody>
                                 <CardTitle>Αθηνά Σταματίου</CardTitle>
                                 <CardSubtitle>Frontend/UI Desinger</CardSubtitle>
-                                <CardText>Write sth</CardText>
+                  
                             </CardBody>
                         </Card>
-                   </Col>
-                   <Col xs={6} md={4}>
+                 
                         <Card>
-                            <CardImg top width="20%" width="150px" src={"/public/nikol.jpg"} alt="Card image cap" />
+                       
+                            <CardImg  width="200" height="260" src={"/public/nikol.jpg"} alt="Card image cap" />
                             <CardBody>
                                 <CardTitle>Νικολέττα Κατσούλη</CardTitle>
                                 <CardSubtitle>Frontend Engineer</CardSubtitle>
-                                <CardText>Write sth</CardText>
+                            
                             </CardBody>
                         </Card>
-                        </Col>
-                        </Row>
-                        <Row>
-                        <Col xs={6} md={4}>
+                       
+                        </CardDeck>
+                        <CardDeck>
+                    
                         <Card>
-                            <CardImg top width="20%" src={"/public/grigoris.jpg"} alt="Card image cap" />
+                            <CardImg width="200" height="260" src={"/public/grigoris.jpg"} alt="Card image cap" />
                             <CardBody>
                                 <CardTitle>Γρηγόρης Θανάσουλας</CardTitle>
                                 <CardSubtitle>Backend Engineer</CardSubtitle>
-                                <CardText>Write sth</CardText>
+                              
                             </CardBody>
                         </Card>
-                        </Col>
-                        <Col xs={6} md={4}>
+                        
                         <Card>
-                            <CardImg top width="20%" width="150px" src={"/public/themis.png"} alt="Card image cap" />
+                            <CardImg width="200" height="260" src={"/public/themis.png"} alt="Card image cap" />
                             <CardBody>
                                 <CardTitle>Θέμης Παπαμελετίου</CardTitle>
                                 <CardSubtitle>Backend Engineer</CardSubtitle>
-                                <CardText>Write sth</CardText>
+                              
                             </CardBody>
                         </Card>
-                        </Col>
-                        </Row>
+                  
+                        </CardDeck>                        
                     </Container>
                 </div>
             </div>
